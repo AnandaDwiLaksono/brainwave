33b330b84b4f71e3784f19f2ae79e89c13133aec
AnandaDwiLaksono <anandad182@gmail.com>
2024-08-23 15:48:16 +0700

    feat: Add scroll-lock npm dependency and update React Router version

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ className, href, onClick, children, px, white }) => {
   const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${px || 'px-7'} ${white ? 'text-n-8' : 'text-n-1'} ${className || ''}`
 
   const renderButton = () => (
-    <button className={classes}>
+    <button className={classes} onClick={onClick}>
       <span className='relative z-10'>{children}</span>
       {ButtonSvg(white)}
     </button>
